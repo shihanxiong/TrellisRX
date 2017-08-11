56133776439c382ceeda6bd24fa18febbb1b97e7
shihanxiong <shihanxiong@gmail.com>
2017-08-10 22:30:21 -0400

    Finished the script to initialize the database with the patients data.

--- myapp/routes/patients.js
+++ myapp/routes/patients.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongodb = require('mongodb');
 
 var uri = "mongodb://pointbit:chickenandrice@pbcluster01-shard-00-00-lviwo.mongodb.net:27017,pbcluster01-shard-00-01-lviwo.mongodb.net:27017,pbcluster01-shard-00-02-lviwo.mongodb.net:27017/TrellisRx?ssl=true&replicaSet=PBCluster01-shard-0&authSource=admin";
+var mongoClient = mongodb.MongoClient;
 
 /* Default page. */
 router.get('/', function (req, res, next) {
@@ -11,8 +12,6 @@ router.get('/', function (req, res, next) {
 
 // Get the list of all patients
 router.get('/showall', function (req, res) {
-  var mongoClient = mongodb.MongoClient;
-
   mongoClient.connect(uri, function (err, db) {
     if (err) {
       console.log('Unable to connect to the database server.', err);
@@ -35,35 +34,70 @@ router.get('/showall', function (req, res) {
   });
 });
 
-// Add a patient
-router.post('/addpatient', function (req, res) {
-  var mongoClient = mongodb.MongoClient;
-
+// Add all patients
+router.get('/addallpatients', function (req, res) {
   mongoClient.connect(uri, function (err, db) {
     if (err) {
       console.log('Unable to connect to the database server.', err);
+      db.close();
     } else {
-      console.log('Connected to server.');
+      console.log('Connection established...');
 
-      var collection = db.collection('patients');
+      var collection = db.collection('Patients');
 
-      var patient = {
-        lastName: req.body.lastName,
-        firstName: req.body.firstName,
-        town: req.body.town
-      };
-
-      collection.insert([patient], function (err, result) {
+      collection.insertMany([{
+        "lastName": "Smith",
+        "firstName": "Fred",
+        "town": "Arlington",
+        "Medications": [{
+            "medname": "morphine",
+            "dose": "10mg",
+            "startDate": new Date("2015-10-10T00:00:00Z")
+          },
+          {
+            "medname": "acetaminophen",
+            "dose": "325mg",
+            "startDate": new Date("2015-10-11T00:00:00Z"),
+            "stopDate": new Date("2015-10-14T00:00:00Z")
+          },
+          {
+            "medname": "furosemide",
+            "dose": "20mg",
+            "startDate": new Date("2015-10-31T00:00:00Z")
+          }
+        ]
+      }, {
+        "lastName": "Jones",
+        "firstName": "Sally",
+        "town": "Milford",
+        "Medications": [{
+            "medname": "morphine",
+            "dose": "15mg",
+            "startDate": new Date("2015-10-11T00:00:00Z")
+          },
+          {
+            "medname": "coumadin",
+            "dose": "5mg",
+            "startDate": new Date("2015-10-15T00:00:00Z")
+          },
+          {
+            "medname": "lovenox",
+            "dose": "100mg/mL",
+            "startDate": new Date("2015-10-31T00:00:00Z"),
+            "stopDate": new Date("2015-11-02T00:00:00Z")
+          }
+        ]
+      }], function (err, result) {
         if (err) {
           console.log(err);
         } else {
           res.redirect('showall');
         }
       });
+
       db.close();
     }
   });
-
 });
 
 module.exports = router;
